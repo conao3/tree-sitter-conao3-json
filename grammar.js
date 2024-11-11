d81cddbc1e38adc265277934d6cd82f748b8ead4
conao3 <conao3@gmail.com>
2024-11-11 13:47:32 +0900

    implement

--- grammar.js
+++ grammar.js
@@ -4,6 +4,14 @@
  * @license Apache-2.0
  */
 
+function sep1(rule, separator) {
+  return seq(rule, repeat(seq(separator, rule)));
+}
+
+function sep(rule, separator) {
+  return optional(sep1(rule, separator));
+}
+
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
@@ -11,7 +19,45 @@ module.exports = grammar({
   name: "conao3_json",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => $._value,
+
+    _value: $ => choice(
+      $.object,
+      $.array,
+      $.string,
+      $.number,
+      $.boolean,
+      $.null,
+    ),
+
+    object: $ => seq('{', sep(seq($.string, ':', $._value), ','), '}'),
+    array: $ => seq('[', sep($._value, ','), ']'),
+
+    string: $ => /"[^"]*"/,
+
+    number: $ => {
+      const sign = /[+-]/
+      const onenine = /[1-9]/
+      const digit = choice('0', onenine)
+      const digits = repeat1(digit)
+      const fraction = seq('.', digits)
+      const integer = seq(
+        optional(sign),
+        digits,
+      )
+      const exponent = seq(
+        /[eE]/,
+        optional(sign),
+        digits
+      )
+      return seq(
+        integer,
+        optional(fraction),
+        optional(exponent)
+      )
+    },
+
+    boolean: $ => choice('true', 'false'),
+    null: $ => 'null',
   }
 });
